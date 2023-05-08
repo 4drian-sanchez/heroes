a42b1a536281ed908ea44de94104936cdc8a6d94
Adrian <adrian.s.duque@gmail.com>
2023-05-08 17:59:47 -0400

    Error de rutas privadas solucionado

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -10,7 +10,7 @@ const init = () => {
     const user = JSON.parse(localStorage.getItem('user'));
 
     return {
-        looged: !!user,
+        logged: !!user,
         user
     }
 }
@@ -18,7 +18,7 @@ const init = () => {
 export const AuthProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(authReducer, {}, init);
-    
+
     const login = (name = '') => {
         const user = { id: 'ABC', name };
 
@@ -27,21 +27,17 @@ export const AuthProvider = ({ children }) => {
             payload: user
         }
 
-        localStorage.setItem( 'user', JSON.stringify( user ) )
-        
+        localStorage.setItem('user', JSON.stringify(user))
+
         dispatch(action);
     }
 
     const logout = () => {
         localStorage.removeItem('user');
-
-
-        
         const action = { type: types.logout };
-        
         dispatch(action);
     }
-    
+
     return (
         <AuthContext.Provider
             value={{
--- src/router/PrivateRuoter.jsx
+++ src/router/PrivateRuoter.jsx
@@ -1,16 +1,17 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, } from 'react';
 import { Navigate } from 'react-router-dom';
-
 import { AuthContext } from '../auth/context/AuthContext';
+
 
 export const PrivateRuoter = ({ children }) => {
 
   const { state } = useContext(AuthContext);
 
+  console.log(state.logged)
   return (
     <>
       {
-        (state.looged)
+        (state.logged)
           ? children
           : <Navigate to="/login" />
       }
